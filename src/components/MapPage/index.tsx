85510e74dfd4f2852bfbb2b97e92eb16a614e5a8
Pomin Wu <pomin5@gmail.com>
2018-04-26 18:54:33 +0800

    Change map size in map page

--- src/components/MapPage/index.tsx
+++ src/components/MapPage/index.tsx
@@ -1,17 +1,14 @@
 import * as React from 'react';
-import { Container } from 'semantic-ui-react';
 import Map from '../../containers/Map';
 
 class MapPage extends React.Component {
   render() {
     return (
-      <Container text>
-        <Map
-          id="cjgdo1yhj0si501559s0hgw2a"
-          width={960}
-          height={600}
-        />
-      </Container>
+      <Map
+        id="cjgdo1yhj0si501559s0hgw2a"
+        width={1960}
+        height={1200}
+      />
     );
   }
 }
