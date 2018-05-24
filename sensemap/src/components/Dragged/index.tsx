9df93b6224225a230b69f14447ef887173227ff9
Pomin Wu <pomin5@gmail.com>
2018-05-24 11:39:47 +0800

    Add Dragged component

---
+++ sensemap/src/components/Dragged/index.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import './index.css';
+
+export interface Props {
+  children: JSX.Element;
+}
+
+export class Dragged extends React.Component<Props> {
+  el: HTMLElement;
+
+  constructor(props: Props) {
+    super(props);
+    this.el = document.createElement('div');
+  }
+
+  componentWillMount() {
+    const root = document.getElementById('modal-root');
+    if (!!root) {
+      root.appendChild(this.el);
+    }
+  }
+
+  componentWillUnmount() {
+    const root = document.getElementById('modal-root');
+    if (!!root) {
+      root.removeChild(this.el);
+    }
+  }
+
+  render() {
+    return ReactDOM.createPortal(
+      this.props.children,
+      this.el,
+    );
+  }
+}
+
+export default Dragged;
