7d8f7e93e49ae40fa3d3811ca324f615d6c7b2ef
caasi Huang <caasi.igd@gmail.com>
2018-06-01 09:57:19 +0800

    Fetch and store annotations for #62

--- sensemap/src/types/importer.ts
+++ sensemap/src/types/importer.ts
@@ -1,6 +1,12 @@
+import { Dispatch } from '.';
 import { ActionUnion, emptyAction } from './action';
 // XXX: should use our hypothesis-api
+import * as API from './hypothesis/api';
 import { Annotation } from './hypothesis/annotation';
+import { findIndex } from 'ramda';
+import { Arr } from './utils';
+
+const h = new API.Hypothesis('https://sense.tw/api');
 
 enum SourceType {
   HYPOTHESIS = 'HYPOTHESIS',
@@ -12,11 +18,19 @@ interface AnnotationLog {
   annotations: Annotation[];
 }
 
+const annotationLog = (url: string): AnnotationLog => ({
+  sourceType: SourceType.HYPOTHESIS,
+  url,
+  annotations: [],
+});
+
 /**
  * The sum of our logs. Maybe we will have more sources in the future.
  */
 type ImportLog
   = AnnotationLog;
+
+const findByUrl = (url: string) => findIndex((a: ImportLog) => a.url === url);
 
 const CHANGE_URL = 'IMPORTER_CHANGE_URL';
 const changeUrl =
@@ -25,11 +39,55 @@ const changeUrl =
     payload: { url },
   });
 
-export const actions = {
+const ANNOTATION_REQUEST = 'IMPORTER_ANNOTATION_REQUEST';
+const annotationRequest =
+  (url: string) => ({
+    type: ANNOTATION_REQUEST as typeof ANNOTATION_REQUEST,
+    payload: { url },
+  });
+
+const ANNOTATION_SUCCESS = 'IMPORTER_ANNOTATION_SUCCESS';
+const annotationSuccess =
+  (url: string, annotations: Annotation[]) => ({
+    type: ANNOTATION_SUCCESS as typeof ANNOTATION_SUCCESS,
+    payload: { url, annotations },
+  });
+
+const ANNOTATION_FAILURE = 'IMPORTER_ANNOTATION_FAILURE';
+const annotationFailure =
+  (url: string, error: Error) => ({
+    type: ANNOTATION_FAILURE as typeof ANNOTATION_FAILURE,
+    payload: { url, error },
+  });
+
+const fetchAnnotations
+  : (url: string) => (dispatch: Dispatch) => Promise<Annotation[]>
+  = (url) => async (dispatch) => {
+    dispatch(annotationRequest(url));
+
+    try {
+      const result = await h.search({ url });
+      dispatch(annotationSuccess(url, result.rows));
+      return result.rows;
+    } catch (error) {
+      dispatch(annotationFailure(url, error));
+      throw error;
+    }
+  };
+
+export const syncActions = {
   changeUrl,
+  annotationRequest,
+  annotationSuccess,
+  annotationFailure,
 };
 
-export type Action = ActionUnion<typeof actions>;
+export const actions = {
+  ...syncActions,
+  fetchAnnotations,
+};
+
+export type Action = ActionUnion<typeof syncActions>;
 
 export interface State {
   url: string;
@@ -46,6 +104,52 @@ export const reducer = (state: State = initial, action: Action = emptyAction): S
     case CHANGE_URL: {
       const { url } = action.payload;
       return { ...state, url };
+    }
+    case ANNOTATION_REQUEST: {
+      const { url } = action.payload;
+      const { logs } = state;
+      const i = findByUrl(url)(logs);
+      const ann = logs[i];
+
+      return {
+        ...state,
+        logs: Arr.replaceOrAppend(logs, i, ann || annotationLog(url)),
+      };
+    }
+    case ANNOTATION_SUCCESS: {
+      const { url, annotations } = action.payload;
+      const { logs } = state;
+      const i = findByUrl(url)(logs);
+      let ann = logs[i];
+
+      if (ann === undefined) {
+        // tslint:disable-next-line:no-console
+        console.error(`Annotation request ${url} not found.`);
+        return state;
+      }
+
+      ann = {
+        ...ann,
+        annotations
+      };
+
+      return {
+        ...state,
+        logs: Arr.replaceOrAppend(logs, i, ann),
+      };
+    }
+    case ANNOTATION_FAILURE: {
+      const { url, error } = action.payload;
+      const { logs } = state;
+      const i = findByUrl(url)(logs);
+
+      // tslint:disable-next-line:no-console
+      console.error(error);
+
+      return {
+        ...state,
+        logs: Arr.remove(logs, i),
+      };
     }
     default:
       return state;
