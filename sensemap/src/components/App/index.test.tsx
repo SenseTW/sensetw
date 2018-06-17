b0804f3931d6573281e05951e1265ea62c0b713d
caasi Huang <caasi.igd@gmail.com>
2018-06-18 02:44:46 +0800

    Test the App component with enzyme

--- sensemap/src/components/App/index.test.tsx
+++ sensemap/src/components/App/index.test.tsx
@@ -1,8 +1,21 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { configure, shallow } from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-16';
 import App from './index';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+configure({ adapter: new Adapter() });
+
+function setup() {
+  const enzymeWrapper = shallow(<App />);
+  return { enzymeWrapper };
+}
+
+describe('components', () => {
+  describe('App', () => {
+    it('should render self without any error', () => {
+      const { enzymeWrapper } = setup();
+
+      expect(enzymeWrapper.find('div').hasClass('App')).toBe(true);
+    });
+  });
 });
