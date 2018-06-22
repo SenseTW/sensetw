a9c96ea259e74be579a6657d18fa4ce7dda1b9a9
YWChen <wenchen.x@gmail.com>
2018-06-22 11:22:20 +0800

    implement sensemap-backend config builder

---
+++ builder/sensemap-backend/scripts/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+cp /templates/* /workspace/
+cd /workspace
+
+BRANCH_NAME=$1
+COMMIT_SHA=$2
+
+sed -i "s/\${BRANCH_NAME}/$BRANCH_NAME/g" env.yaml
+sed -i "s/\${BRANCH_NAME}/$BRANCH_NAME/g" sensemap-backend.yaml
+sed -i "s/\${COMMIT_SHA}/$COMMIT_SHA/g" sensemap-backend.yaml
+
