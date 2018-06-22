5d9ab80d48601cc3755ae6c9cd66dcb9c503b9f9
YWChen <wenchen.x@gmail.com>
2018-06-22 15:33:40 +0800

    Fixed master branch db config

--- builder/sensemap-backend/scripts/build.sh
+++ builder/sensemap-backend/scripts/build.sh
@@ -5,7 +5,12 @@ cd /workspace
 BRANCH_NAME=$1
 COMMIT_SHA=$2
 
-sed -i "s/\${BRANCH_NAME}/$BRANCH_NAME/g" env.yaml
+DB_NAME=sensemap
+if [ "$1" != "release" ] && [ "$1" != "master" ]; then
+  DB_NAME="sensemap_${BRANCH_NAME}"
+fi
+
+sed -i "s/\${DB_NAME}/$DB_NAME/g" env.yaml
 sed -i "s/\${BRANCH_NAME}/$BRANCH_NAME/g" sensemap-backend.yaml
 sed -i "s/\${COMMIT_SHA}/$COMMIT_SHA/g" sensemap-backend.yaml
 sed -i "s/\${BRANCH_NAME}/$BRANCH_NAME/g" sensemap-backend_service.yaml
