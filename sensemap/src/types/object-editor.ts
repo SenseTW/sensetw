e2d9957ba0a70fa2c5305a9a3311019aaf883fec
caasi Huang <caasi.igd@gmail.com>
2018-07-02 06:13:31 +0800

    Implement the map editor

--- sensemap/src/types/object-editor.ts
+++ sensemap/src/types/object-editor.ts
@@ -1,4 +1,5 @@
 import { ActionUnion, emptyAction } from './action';
+import { MapID } from './sense/map';
 import * as F from './sense/focus';
 
 export enum StatusType {
@@ -20,9 +21,17 @@ const focusObject =
     payload: { focus },
   });
 
+const FOCUS_MAP = 'FOCUS_MAP';
+const focusMap =
+  (map?: MapID) => ({
+    type: FOCUS_MAP as typeof FOCUS_MAP,
+    payload: { map },
+  });
+
 export const actions = {
   changeStatus,
   focusObject,
+  focusMap,
 };
 
 export type Action = ActionUnion<typeof actions>;
@@ -30,11 +39,13 @@ export type Action = ActionUnion<typeof actions>;
 export type State = {
   status: StatusType,
   focus: F.Focus,
+  map?: MapID,
 };
 
 export const initial: State = {
   status: StatusType.HIDE,
   focus: F.focusNothing(),
+  map: undefined,
 };
 
 export const reducer = (state: State = initial, action: Action = emptyAction) => {
@@ -48,6 +59,11 @@ export const reducer = (state: State = initial, action: Action = emptyAction) =>
       const { focus } = action.payload;
 
       return { ...state, focus };
+    }
+    case FOCUS_MAP: {
+      const { map } = action.payload;
+
+      return { ...state, map };
     }
     default:
       return state;
