b0634f8e27841e3d39e4d5bad6d44ae307f95633
caasi Huang <caasi.igd@gmail.com>
2018-07-02 16:31:47 +0800

    Add menu links for #96 and #97

--- sensemap/src/components/Header/index.tsx
+++ sensemap/src/components/Header/index.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 import { Menu, Icon, Dropdown } from 'semantic-ui-react';
 import Breadcrumb from './Breadcrumb';
 import Submenu from './Submenu';
+import * as R from '../../types/routes';
 import './index.css';
 const logo = require('./logo.png');
 
@@ -18,10 +20,17 @@ class Header extends React.Component<Props> {
     return (
       <div className="sense-header">
         <Menu inverted>
-          <Menu.Item>
+          <Menu.Item as={Link} to={R.index}>
             <img src={logo} />
           </Menu.Item>
-          <Menu.Item>ABOUT</Menu.Item>
+          <Menu.Item
+            as="a"
+            href="https://beta.hackfoldr.org/sense-tw"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            ABOUT
+          </Menu.Item>
           <Menu.Menu position="right">
             <Menu.Item>
               <Icon name="question circle outline" size="large" />
