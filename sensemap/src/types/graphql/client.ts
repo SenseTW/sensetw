82445f21cd67ea87e790959ce851d9481de008e9
YWChen <wenchen.x@gmail.com>
2018-07-21 13:15:34 +0800

    Fixed api-server env

--- sensemap/src/types/graphql/client.ts
+++ sensemap/src/types/graphql/client.ts
@@ -5,5 +5,5 @@ function noSlash(a: string): string {
     return a.replace(/\/+$/, '');
   }
 
-export const endpoint = noSlash(process.env.SENSEMAP_API_ROOT || 'https://api.sense.tw/graphql');
+export const endpoint = noSlash(process.env.REACT_APP_SENSEMAP_API_ROOT || 'https://api.sense.tw/graphql');
 export const client = new GraphQLClient(endpoint);
