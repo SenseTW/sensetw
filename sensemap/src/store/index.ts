2f480ca06407068998024c7539b1b6893dccf56a
陳信屹 <ossug.hychen@gmail.com>
2018-07-23 10:26:05 +0800

    sensemap: Init session service.

--- sensemap/src/store/index.ts
+++ sensemap/src/store/index.ts
@@ -2,6 +2,7 @@ import { applyMiddleware, createStore } from 'redux';
 import { reducer } from '../types';
 import logger from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
+import { sessionService } from 'redux-react-session';
 
 export const store = createStore(
   reducer,
@@ -14,3 +15,5 @@ export const store = createStore(
       thunkMiddleware
     )
 );
+
+sessionService.initSessionService(store);
