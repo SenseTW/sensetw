e23776c017aa45d02e0eab9f1d6fbca3c7e3f4a2
caasi Huang <caasi.igd@gmail.com>
2018-07-26 14:02:22 +0800

    Implement selected indicators for whole map cards and boxes

--- sensemap/src/components/WholeMap/Box/index.tsx
+++ sensemap/src/components/WholeMap/Box/index.tsx
@@ -1,13 +1,29 @@
 import * as React from 'react';
-import { Rect } from 'react-konva';
+import { Group, Rect } from 'react-konva';
 import { TransformerForProps } from '../';
-import { BoxData } from '../../../types';
+import { ObjectData, BoxData } from '../../../types';
 import { BoundingBox } from '../../../graphics/drawing';
 import { transformObject } from '../../../types/viewport';
+import { noop } from '../../../types/utils';
+import { Event as KonvaEvent } from '../../../types/konva';
 
-type Props = BoundingBox & BoxData & TransformerForProps;
+interface OwnProps {
+  mapObject: ObjectData;
+  selected?: boolean;
+  onSelect?(e: KonvaEvent.Mouse, object: ObjectData): void;
+  onDeselect?(e: KonvaEvent.Mouse, object: ObjectData): void;
+  onDragStart?(e: KonvaEvent.Mouse, object: ObjectData): void;
+  onDragMove?(e: KonvaEvent.Mouse, object: ObjectData): void;
+  onDragEnd?(e: KonvaEvent.Mouse, object: ObjectData): void;
+}
 
-class Box extends React.PureComponent<Props> {
+interface OwnState {
+  newlySelected: boolean;
+}
+
+type Props = OwnProps & BoundingBox & BoxData & TransformerForProps;
+
+class Box extends React.PureComponent<Props, OwnState> {
   static style = {
     backgroundColor: '#4d4d4d',
     borderRadius: 18,
@@ -19,10 +35,31 @@ class Box extends React.PureComponent<Props> {
       bottom: 12,
       left: 9,
     },
+    selected: {
+      borderRadius: 36,
+      color: '#3ad8fa',
+      offset: {
+        x: -18,
+        y: -18,
+      },
+      strokeWidth: 9,
+    }
+  };
+
+  state = {
+    newlySelected: false,
   };
 
   render() {
-    const { transform } = this.props;
+    const {
+      selected = false,
+      transform, mapObject,
+      onSelect = noop,
+      onDeselect = noop,
+      onDragStart = noop,
+      onDragMove = noop,
+      onDragEnd = noop
+    } = this.props;
     const { x, y, width, height } = transform({
       x: this.props.x,
       y: this.props.y,
@@ -31,15 +68,37 @@ class Box extends React.PureComponent<Props> {
     });
     const style = transformObject(transform, Box.style) as typeof Box.style;
 
-    return (
+    const selectedWidth = width  - style.selected.offset.x * 2;
+    const selectedHeight = height - style.selected.offset.y * 2;
+    const selectedRect = selected && (
       <Rect
-        x={x}
-        y={y}
-        width={width}
-        height={height}
-        cornerRadius={style.borderRadius}
-        fill={style.backgroundColor}
+        x={style.selected.offset.x}
+        y={style.selected.offset.y}
+        width={selectedWidth}
+        height={selectedHeight}
+        cornerRadius={style.selected.borderRadius}
+        stroke={style.selected.color}
+        strokeWidth={style.selected.strokeWidth}
       />
+    );
+
+    return (
+      <Group x={x} y={y}>
+        {selectedRect}
+        <Rect
+          width={width}
+          height={height}
+          cornerRadius={style.borderRadius}
+          fill={style.backgroundColor}
+          onMouseDown={
+            (e: KonvaEvent.Mouse) =>
+              selected ? onDeselect(e, mapObject) : onSelect(e, mapObject)
+          }
+          onDragStart={(e: KonvaEvent.Mouse) => onDragStart(e, mapObject)}
+          onDragMove={(e: KonvaEvent.Mouse) => onDragMove(e, mapObject)}
+          onDragEnd={(e: KonvaEvent.Mouse) => onDragEnd(e, mapObject)}
+        />
+      </Group>
     );
   }
 }
