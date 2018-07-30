a22cad25740b69d1031dcd33495b0c39e81faf2d
caasi Huang <caasi.igd@gmail.com>
2018-07-30 19:04:26 +0800

    Draw the caret

--- sensemap/src/components/WholeMap/CardList/index.tsx
+++ sensemap/src/components/WholeMap/CardList/index.tsx
@@ -1,9 +1,80 @@
 import * as React from 'react';
-import { Group, Rect } from 'react-konva';
+import { Path, Group, Rect } from 'react-konva';
 import { TransformerForProps } from '../';
 import { ObjectData, CardData } from '../../../types';
 import { transformObject } from '../../../types/viewport';
 import TextList from '../../Layout/TextList';
+
+interface CaretProps {
+  top?: boolean;
+  right?: boolean;
+  bottom?: boolean;
+  left?: boolean;
+  x?: number;
+  y?: number;
+  base?: number;
+  height?: number;
+  fill: string;
+}
+
+class Caret extends React.PureComponent<CaretProps> {
+  render() {
+    let { top } = this.props;
+    const {
+      right,
+      bottom,
+      left,
+      x = 0,
+      y = 0,
+      base = 0,
+      height = 0,
+      fill,
+    } = this.props;
+    if (!top && !right && !left && !bottom) { top = true; }
+
+    if (base <= 0 || height <= 0) { return null; }
+
+    if (top) {
+      return (
+        <Path
+           x={x}
+           y={y}
+           data={`M${0} ${-height} L${base / 2} ${0} h${-base} L${0}, ${-height}z`}
+           fill={fill}
+        />
+      );
+    } else if (right) {
+      return (
+        <Path
+           x={x}
+           y={y}
+           data={`M${height} ${0} L${0} ${base / 2} v${-base} L${height} ${0}z`}
+           fill={fill}
+        />
+      );
+    } else if (bottom) {
+      return (
+        <Path
+           x={x}
+           y={y}
+           data={`M${0} ${height} L${-base / 2} ${0} h${base} L${0} ${height}z`}
+           fill={fill}
+        />
+      );
+    } else if (left) {
+      return (
+        <Path
+           x={x}
+           y={y}
+           data={`M${-height} ${0} L${0} ${-base / 2} v${base} L${-height} ${0}z`}
+           fill={fill}
+        />
+      );
+    } else {
+      return null as never;
+    }
+  }
+}
 
 interface OwnProps {
   mapObject: ObjectData;
@@ -12,8 +83,16 @@ interface OwnProps {
   cards: CardData[];
 }
 
+enum Direction {
+  BOTTOM_RIGHT = 'bottom right',
+  BOTTOM_LEFT = 'bottom left',
+  TOP_RIGHT = 'top right',
+  TOP_LEFT = 'top left',
+}
+
 interface OwnState {
   height: number;
+  direction: Direction;
 }
 
 type Props = OwnProps & TransformerForProps;
@@ -22,6 +101,14 @@ class CardList extends React.PureComponent<Props, OwnState> {
   static style = {
     backgroundColor: '#b3e5fc',
     borderRadius: 18,
+    caret: {
+      offset: {
+        x: 1,
+        y: 12,
+      },
+      height: 24,
+      base: 24 * Math.sqrt(1 / 3) * 2,
+    },
     color: '#000',
     fontSize: 36,
     width: 630,
@@ -39,6 +126,7 @@ class CardList extends React.PureComponent<Props, OwnState> {
 
   state = {
     height: 0,
+    direction: Direction.BOTTOM_RIGHT,
   };
 
   handleResize = (width: number, height: number) => {
@@ -54,12 +142,12 @@ class CardList extends React.PureComponent<Props, OwnState> {
       width: CardList.style.width,
     });
     const textWidth = width - style.padding.left - style.padding.right;
-    const { height: textHeight } = this.state;
+    const { height: textHeight, direction } = this.state;
     const height = textHeight + style.padding.top + style.padding.bottom;
     const cardTexts = cards.map(c => c.summary || c.title);
 
-    return (
-      <Group x={x + style.margin.left} y={y}>
+    const renderedText = (
+      <React.Fragment>
         <Rect
           width={width}
           height={height}
@@ -75,8 +163,69 @@ class CardList extends React.PureComponent<Props, OwnState> {
           texts={cardTexts}
           onResize={this.handleResize}
         />
-      </Group>
+      </React.Fragment>
     );
+
+    switch (direction) {
+      case Direction.BOTTOM_RIGHT:
+        return (
+          <Group x={x + style.margin.left} y={y}>
+            <Caret
+              left
+              x={style.caret.offset.x}
+              y={style.caret.offset.y + style.caret.base / 2}
+              base={style.caret.base}
+              height={style.caret.height}
+              fill={style.backgroundColor}
+            />
+            {renderedText}
+          </Group>
+        );
+      case Direction.BOTTOM_LEFT:
+        return (
+          <Group x={x - width - style.margin.left} y={y}>
+            <Caret
+              right
+              x={width - style.caret.offset.x}
+              y={style.caret.offset.y + style.caret.base / 2}
+              base={style.caret.base}
+              height={style.caret.height}
+              fill={style.backgroundColor}
+            />
+            {renderedText}
+          </Group>
+        );
+      case Direction.TOP_RIGHT:
+        return (
+          <Group x={x + style.margin.left} y={y - height}>
+            <Caret
+              left
+              x={style.caret.offset.x}
+              y={height - style.caret.offset.y - style.caret.base / 2}
+              base={style.caret.base}
+              height={style.caret.height}
+              fill={style.backgroundColor}
+            />
+            {renderedText}
+          </Group>
+        );
+      case Direction.TOP_LEFT:
+        return (
+          <Group x={x - width - style.margin.left} y={y - height}>
+            <Caret
+              right
+              x={width - style.caret.offset.x}
+              y={height - style.caret.offset.y - style.caret.base / 2}
+              base={style.caret.base}
+              height={style.caret.height}
+              fill={style.backgroundColor}
+            />
+            {renderedText}
+          </Group>
+        );
+      default:
+        return null as never;
+    }
   }
 }
 
