25767a71cb251a55a8ba52e5567692004f6c39a5
YWChen <wenchen.x@gmail.com>
2018-07-31 12:10:44 +0800

    Move session-secret to build env

--- builder/sensemap-backend/scripts/build.sh
+++ builder/sensemap-backend/scripts/build.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
-cp /templates/* /workspace/
-cd /workspace
+#cp /templates/* /workspace/
+#cd /workspace
 
 BRANCH_NAME=$1
 COMMIT_SHA=$2
 DB_ACCOUNT=$3
 DB_PASSWORD=$4
+SESSION_SECRET=$5
 
-PUBLIC_URL=https://staging.api.sense.tw/
+PUBLIC_URL=https\\:\\/\\/staging.api.sense.tw\\/
 DB_NAME=sensemap
 if [ "$1" != "release" ] && [ "$1" != "master" ]; then
   DB_NAME="sensemap_${BRANCH_NAME}"
-  PUBLIC_URL="https://${BRANCH_NAME}.staging.api.sense.tw/"
+  PUBLIC_URL="https\\:\\/\\/${BRANCH_NAME}.staging.api.sense.tw\\/"
+fi
+
+if [ -z "$SESSION_SECRET" ]; then
+  SESSION_SECRET="TEST"
 fi
 
 sed -i "s/\${DB_NAME}/$DB_NAME/g" env.yaml
