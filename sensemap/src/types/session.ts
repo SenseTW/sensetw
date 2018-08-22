acfcb3e1e048be3707a08cba6111d7db3b96a6e7
Pomin Wu <pomin5@gmail.com>
2018-08-22 12:51:09 +0800

    Fix code style

--- sensemap/src/types/session.ts
+++ sensemap/src/types/session.ts
@@ -1,18 +1,18 @@
 import { State, sessionReducer } from 'redux-react-session';
 
 export type User = {
-    id?: string,
-    email?: string,
-    username?: string,
+  id?: string,
+  email?: string,
+  username?: string,
 };
 
 export type State = State;
 
 export const inital: State = {
-    checked: false,
-    authenticated: false,
-    invalid: false,
-    user: {}
+  checked: false,
+  authenticated: false,
+  invalid: false,
+  user: {}
 };
 
 export const reducer = sessionReducer;
