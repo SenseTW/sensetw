db06181df83e2decfdd09fa6996a661b9a00eb87
Pomin Wu <pomin5@gmail.com>
2018-08-23 08:20:50 +0800

    Add card owner in frontend

--- sensemap/src/components/Inbox/AddCardButton/index.tsx
+++ sensemap/src/components/Inbox/AddCardButton/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as C from '../../../types/sense/card';
+import * as SN from '../../../types/session';
 import { ActionProps } from '../../../types';
 import { Button } from 'semantic-ui-react';
 import './index.css';
@@ -7,19 +8,20 @@ import './index.css';
 interface StateFromProps {
   mapId: string;
   visible: boolean;
+  session: SN.State;
 }
 
 export type Props = StateFromProps & ActionProps;
 
-export default function AddCardButton({mapId, visible, actions: acts}: Props) {
+export default function AddCardButton({mapId, visible, session, actions: acts}: Props) {
   function onClick() {
-        acts.senseObject.createCard(mapId, C.cardData());
+        acts.senseObject.createCard(mapId, C.cardData(), session.user);
         acts.senseMap.activateInboxPage(1);
   }
   const className = visible ? 'inbox__add-card-btn' : 'inbox__add-card-btn hidden';
   return (
     <div className={className}>
-      <Button icon="plus" size="tiny" color="grey" onClick={onClick} /> 
+      <Button icon="plus" size="tiny" color="grey" onClick={onClick} />
     </div>
   );
 }
--- sensemap/src/components/Inbox/index.tsx
+++ sensemap/src/components/Inbox/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Pagination, PaginationProps, Modal, Button } from 'semantic-ui-react';
 import { CardData, State, ActionProps } from '../../types';
+import * as SN from '../../types/session';
 import Sources from '../../containers/Sources';
 import AddCardButton from './AddCardButton';
 import SyncButton from './SyncButton';
@@ -12,13 +13,14 @@ import './index.css';
 export interface StateFromProps {
   cards: CardData[];
   senseMap: State['senseMap'];
+  session: SN.State;
 }
 
 export interface OwnProps {}
 
 export type Props = StateFromProps & ActionProps & OwnProps;
 
-export function Inbox({ cards, senseMap, actions: acts }: Props) {
+export function Inbox({ cards, senseMap, session, actions: acts }: Props) {
   const mapId = senseMap.map;
   return (
     <div className="inbox">
@@ -40,7 +42,7 @@ export function Inbox({ cards, senseMap, actions: acts }: Props) {
           </Modal.Content>
         </Modal>
       </div>
-      <AddCardButton mapId={mapId} visible={senseMap.activateInboxPage === 1} actions={acts} />
+      <AddCardButton mapId={mapId} visible={senseMap.activateInboxPage === 1} actions={acts} session={session} />
       <Pager data={cards} pageSize={9} currentPage={senseMap.activateInboxPage} >
         {({ data, totalPages, currentPage, handlePageChange }) => {
           return (
