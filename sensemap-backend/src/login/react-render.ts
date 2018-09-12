8076fc9116b184b3f148c9b73f702b60acc15546
Pomin Wu <pomin5@gmail.com>
2018-09-12 12:33:01 +0800

    Add GTM

--- sensemap-backend/src/login/react-render.ts
+++ sensemap-backend/src/login/react-render.ts
@@ -4,6 +4,6 @@ import * as fs from 'async-file';
 export async function render(component, props = {}) {
   const html = ReactDOMServer.renderToStaticMarkup(component(props));
   const data = await fs.readFile(__dirname + '/../../public/index.html', 'utf8');
-  const document = data.replace(/<body>[\s\S]*<\/body>/, `<body>${html}</body>`);
+  const document = data.replace(/<div id="root">[\s\S]*<\/div>/, `${html}`);
   return document;
 }
