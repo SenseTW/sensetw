ea977adcf65db9dc27f76f66e9d6597d896380ce
YWChen <wenchen.x@gmail.com>
2018-09-12 18:40:45 +0800

    remove useless files

--- builder/docker-build.sh
+++
@@ -1,9 +0,0 @@
-#docker build -t gcr.io/ggv-notetool/sense-builder sensemap/.
-#docker build -t gcr.io/ggv-notetool/api-builder sensemap-backend/.
-#docker run -v $(pwd)/workspace:/workspace gcr.io/ggv-notetool/api-builder release test-commit-sha
-cd ..
-sudo rm -rf sensemap/build sensemap/gcs-build sensemap/node_modules
-docker build -t gcr.io/ggv-notetool/sensemap --build-arg ENV=stage COMMIT_SHA=test-commit-sha BRANCH_NAME=master .
-
-#docker build -t gcr.io/ggv-notetool/sensemap-backend sensemap-backend/.
-
