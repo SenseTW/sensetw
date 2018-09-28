e6badfcdab16d27eb5748d25babea67eca2ad868
Yen-Wen Chen <wenchen.x@gmail.com>
2018-09-28 17:20:02 +0000

    Use pg_isready to check db connection and run migration in every build

--- Dockerfile
+++ Dockerfile
@@ -10,13 +10,17 @@ COPY sensemap/. /workspace
 WORKDIR /workspace
 RUN /opt/front-builder.sh $BRANCH_NAME $COMMIT_SHA $GA_ID $GTM_ID
 
-FROM node:8
+FROM node:8-alpine
+RUN apk --no-cache add \
+        postgresql-client \
+        bash
 RUN mkdir /workspace
 RUN mkdir /static
 COPY sensemap-backend/. /workspace
+COPY builder/sensemap-backend/entrypoint.sh /workspace
 COPY --from=front-builder /workspace/build/. /static
 
 WORKDIR /workspace
 RUN yarn
 
-ENTRYPOINT ["/workspace/scripts/entrypoint.sh"]
+ENTRYPOINT ["/workspace/entrypoint.sh"]
---
+++ builder/sensemap-backend/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+CMD=$1
+
+cd /workspace
+
+if [ "$CMD" = "cloudbuild-staging" ]; then
+    echo "Waiting for Database prepare process"
+    while true; do
+        if [[ -f "/tmp/pod/main-terminated" ]]; then
+	    break
+        fi;
+	sleep 1;
+    done
+fi
+
+echo "Checking Database Connection ..."
+while ! pg_isready -d $DATABASE_URL > /dev/null 2> /dev/null; do
+    echo "Connecting to database Failed."
+    sleep 1
+done
+
+echo "Running Database Migration ..."
+yarn run migrate up
+
+echo "Start Service ..."
+yarn start
