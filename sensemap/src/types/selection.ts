be4bfdd376317d5a88d46e17796236c6fca19a34
caasi Huang <caasi.igd@gmail.com>
2018-10-11 22:51:52 +0800

    feat(selection): Add new selection lists (#311)

--- sensemap/src/types/selection.ts
+++ sensemap/src/types/selection.ts
@@ -1,7 +1,42 @@
 import { ActionUnion } from './action';
 import { BoundingBox, emptyBoundingBox } from '../graphics/drawing';
 import { ObjectID } from './sense/object';
+import { BoxID } from './sense/box';
+import { CardID } from './sense/card';
+import { EdgeID } from './sense/edge';
+import { findIndex, equals } from 'ramda';
 
+/**
+ * Appends something to a list if it's not in the list.
+ *
+ * @param x The thing
+ * @param xs The list
+ */
+const append
+  : <T>(x: T, xs: T[]) => T[]
+  = (x, xs) => {
+    const i = findIndex(equals(x), xs);
+    return i === -1
+      ? [...xs, x]
+      : xs;
+  };
+
+/**
+ * Removes something from a list if it's in the list.
+ *
+ * @param x The thing
+ * @param xs The list
+ */
+const remove
+  : <T>(x: T, xs: T[]) => T[]
+  = (x, xs) => {
+    const i = findIndex(equals(x), xs);
+    return i === -1
+      ? xs
+      : [...xs.slice(0, i), ...xs.slice(i + 1)];
+  };
+
+// old actions
 const ADD_OBJECT_TO_SELECTION = 'ADD_OBJECT_TO_SELECTION';
 const addObjectToSelection = (id: ObjectID) => ({
   type: ADD_OBJECT_TO_SELECTION as typeof ADD_OBJECT_TO_SELECTION,
@@ -20,6 +55,63 @@ const toggleObjectSelection = (id: ObjectID) => ({
   payload: id,
 });
 
+// Selectable items in this application:
+//   * map objects:
+//     * boxes
+//     * cards
+//   * edges
+//   * inbox cards
+//
+// The new selection structure should hold all of them.
+// And map objects should be serialized to the query string.
+const SELECT_MAP_BOX = 'SELECT_MAP_BOX';
+const selectMapBox = (objectId: ObjectID, boxId: BoxID) => ({
+  type: SELECT_MAP_BOX as typeof SELECT_MAP_BOX,
+  payload: { objectId, boxId },
+});
+
+const UNSELECT_MAP_BOX = 'UNSELECT_MAP_BOX';
+const unselectMapBox = (objectId: ObjectID, boxId: BoxID) => ({
+  type: UNSELECT_MAP_BOX as typeof UNSELECT_MAP_BOX,
+  payload: { objectId, boxId },
+});
+
+const SELECT_MAP_CARD = 'SELECT_MAP_CARD';
+const selectMapCard = (objectId: ObjectID, cardId: CardID) => ({
+  type: SELECT_MAP_CARD as typeof SELECT_MAP_CARD,
+  payload: { objectId, cardId },
+});
+
+const UNSELECT_MAP_CARD = 'UNSELECT_MAP_CARD';
+const unselectMapCard = (objectId: ObjectID, cardId: CardID) => ({
+  type: UNSELECT_MAP_CARD as typeof UNSELECT_MAP_CARD,
+  payload: { objectId, cardId },
+});
+
+const SELECT_MAP_EDGE = 'SELECT_MAP_EDGE';
+const selectMapEdge = (edgeId: EdgeID) => ({
+  type: SELECT_MAP_EDGE as typeof SELECT_MAP_EDGE,
+  payload: { edgeId },
+});
+
+const UNSELECT_MAP_EDGE = 'UNSELECT_MAP_EDGE';
+const unselectMapEdge = (edgeId: EdgeID) => ({
+  type: UNSELECT_MAP_EDGE as typeof UNSELECT_MAP_EDGE,
+  payload: { edgeId },
+});
+
+const SELECT_INBOX_CARD = 'SELECT_INBOX_CARD';
+const selectInboxCard = (cardId: CardID) => ({
+  type: SELECT_INBOX_CARD as typeof SELECT_INBOX_CARD,
+  payload: { cardId },
+});
+
+const UNSELECT_INBOX_CARD = 'UNSELECT_INBOX_CARD';
+const unselectInboxCard = (cardId: CardID) => ({
+  type: UNSELECT_INBOX_CARD as typeof UNSELECT_INBOX_CARD,
+  payload: { cardId },
+});
+
 const SET_BOUNDING_BOX = 'SET_BOUNDING_BOX';
 const setBoundingBox = (boundingBox: BoundingBox) => ({
   type: SET_BOUNDING_BOX as typeof SET_BOUNDING_BOX,
@@ -32,9 +124,19 @@ const clearSelection = () => ({
 });
 
 export const actions = {
+  // old actions
   addObjectToSelection,
   removeObjectFromSelection,
   toggleObjectSelection,
+  // new actions
+  selectMapBox,
+  unselectMapBox,
+  selectMapCard,
+  unselectMapCard,
+  selectMapEdge,
+  unselectMapEdge,
+  selectInboxCard,
+  unselectInboxCard,
   setBoundingBox,
   clearSelection,
 };
@@ -42,11 +144,24 @@ export const actions = {
 export type Action = ActionUnion<typeof actions>;
 
 export type State = {
+  // old selections
   objects: ObjectID[],
+  // new selections
+  mapBoxes: { objectId: ObjectID, boxId: BoxID }[],
+  mapCards: { objectId: ObjectID, cardId: CardID }[],
+  mapEdges: EdgeID[],
+  inboxCards: CardID[],
   boundingBox: BoundingBox,
 };
+
 export const initial: State = {
+  // old selections
   objects: [],
+  // new selections
+  mapBoxes: [],
+  mapCards: [],
+  mapEdges: [],
+  inboxCards: [],
   boundingBox: emptyBoundingBox,
 };
 
@@ -95,6 +210,58 @@ export const reducer = (state: State = initial, action: Action): State => {
           objects: [...state.objects, id],
         };
       }
+    }
+    case SELECT_MAP_BOX: {
+      return {
+        ...state,
+        mapBoxes: append(action.payload, state.mapBoxes),
+      };
+    }
+    case SELECT_MAP_CARD: {
+      return {
+        ...state,
+        mapCards: append(action.payload, state.mapCards),
+      };
+    }
+    case SELECT_MAP_EDGE: {
+      const { edgeId } = action.payload;
+      return {
+        ...state,
+        mapEdges: append(edgeId, state.mapEdges),
+      };
+    }
+    case SELECT_INBOX_CARD: {
+      const { cardId } = action.payload;
+      return {
+        ...state,
+        inboxCards: append(cardId, state.inboxCards),
+      };
+    }
+    case UNSELECT_MAP_BOX: {
+      return {
+        ...state,
+        mapBoxes: remove(action.payload, state.mapBoxes),
+      };
+    }
+    case UNSELECT_MAP_CARD: {
+      return {
+        ...state,
+        mapCards: remove(action.payload, state.mapCards),
+      };
+    }
+    case UNSELECT_MAP_EDGE: {
+      const { edgeId } = action.payload;
+      return {
+        ...state,
+        mapEdges: remove(edgeId, state.mapEdges),
+      };
+    }
+    case UNSELECT_INBOX_CARD: {
+      const { cardId } = action.payload;
+      return {
+        ...state,
+        inboxCards: remove(cardId, state.inboxCards),
+      };
     }
     case CLEAR_SELECTION: {
       return {
