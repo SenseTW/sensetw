3e785b5c36f2d2787e3b57de193f66b905da2750
caasi Huang <caasi.igd@gmail.com>
2018-10-19 06:51:27 +0800

    feat(Edge): Show the direction indicators (#315)

--- sensemap/src/components/Map/Edge/index.tsx
+++ sensemap/src/components/Map/Edge/index.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
-import { Group, Line } from 'react-konva';
+import { Group, Line, Circle } from 'react-konva';
 import { TransformerForProps } from '../../Layout';
 import Selectable from '../../Layout/Selectable';
 import * as G from '../../../graphics/point';
 import { Edge as EdgeData } from '../../../types';
+import { transformObject } from '../../../types/viewport';
 import { noop } from '../../../types/utils';
+
+const SQRT3 = 1.73205;
 
 export interface Props extends TransformerForProps {
   from:      G.Point;
@@ -21,6 +24,39 @@ export interface Props extends TransformerForProps {
   // tslint:enable
 }
 
+interface IndicatorProps {
+  x: number;
+  y: number;
+  radius: number;
+  rotation: number;
+  color: string;
+  backgroundColor: string;
+}
+
+function Indicator(props: IndicatorProps) {
+  const { radius, rotation, x, y, color, backgroundColor } = props;
+  const innerRadius = radius * 0.75;
+
+  return (
+    <Group x={x} y={y}>
+      <Circle
+        radius={radius}
+        fill={backgroundColor}
+      />
+      <Line
+        closed
+        points={[
+          -innerRadius / 2, -innerRadius / 2 * SQRT3,
+          innerRadius, 0,
+          -innerRadius / 2, innerRadius / 2 * SQRT3,
+        ]}
+        rotation={rotation}
+        fill={color}
+      />
+    </Group>
+  );
+}
+
 class Edge extends React.PureComponent<Props> {
   static style = {
     color: '#000000',
@@ -31,13 +67,31 @@ class Edge extends React.PureComponent<Props> {
     selected: {
       color: '#3ad8fa',
     },
+    indicator: {
+      radius: 6,
+      color: '#ffffff',
+      distance: 100,
+    }
   };
 
   render () {
     const { transform, data, selected } = this.props;
-    const style = Edge.style;
+    const style = transformObject(transform, Edge.style) as typeof Edge.style;
     const from = G.toTuple(transform(this.props.from));
     const to = G.toTuple(transform(this.props.to));
+    const centerX = (from[0] + to[0]) / 2;
+    const centerY = (from[1] + to[1]) / 2;
+    const dX = (to[0] - from[0]);
+    const dY = (to[1] - from[1]);
+    const showMoreIndicators = dX * dX + dY * dY > 4 * style.indicator.distance * style.indicator.distance;
+    const radius = Math.atan2(dY, dX);
+    const rotation = radius * 180 / Math.PI;
+    const x100 = Math.cos(radius) * style.indicator.distance;
+    const y100 = Math.sin(radius) * style.indicator.distance;
+    const prevX = centerX - x100;
+    const prevY = centerY - y100;
+    const nextX = centerX + x100;
+    const nextY = centerY + y100;
     const onSelect    = this.props.onSelect    || noop;
     const onDeselect  = this.props.onDeselect  || noop;
     const onMouseOver = this.props.onMouseOver || noop;
@@ -72,6 +126,36 @@ class Edge extends React.PureComponent<Props> {
             onMouseOver={(e) => onMouseOver(e, data)}
             onMouseOut={(e) => onMouseOut(e, data)}
           />
+          {
+            showMoreIndicators &&
+            <Indicator
+              x={prevX}
+              y={prevY}
+              radius={style.indicator.radius}
+              rotation={rotation}
+              color={style.indicator.color}
+              backgroundColor={selected ? style.selected.color : style.color}
+            />
+          }
+          <Indicator
+            x={centerX}
+            y={centerY}
+            radius={style.indicator.radius}
+            rotation={rotation}
+            color={style.indicator.color}
+            backgroundColor={selected ? style.selected.color : style.color}
+          />
+          {
+            showMoreIndicators &&
+            <Indicator
+              x={nextX}
+              y={nextY}
+              radius={style.indicator.radius}
+              rotation={rotation}
+              color={style.indicator.color}
+              backgroundColor={selected ? style.selected.color : style.color}
+            />
+          }
         </Group>
       </Selectable>
     );
